e2de18dba3857c05a3d27088d399bfc77dddd202
yasminzeq-sys <yasminzeq@gmail.com>
2026-04-08 16:06:29 +0800

    Refine output messages for user feedback
    Updated the display messages for AI status and popularity alert.

--- app.py
+++ app.py
@@ -61,7 +61,8 @@ with col2:
     # CHANGE THIS LINE BELOW AS WELL:
     if hesitant_users:
         st.info("💡 **AI Recommendation:** Show the 'Popularity Alert'!")
-        st.toast("Wait! 50 other people bought this today.")
+        st.toast("High Demand! 50 other people bought this today.")
+        st.warning("### Don't Miss Out!")
         st.image("tablet.jpeg", width=300)
     else:
         st.write("No popularity alert needed. Monitoring activity...")
